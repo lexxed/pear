bfc700871a07723b8196dc0d8b0fae1891ce8bb7
lexxed <lexx.tham@gmail.com>
2018-10-12 11:36:54 +0000

    packages ad

--- resources/views/travel-packages.blade.php
+++ resources/views/travel-packages.blade.php
@@ -78,7 +78,18 @@
           <p class="autoplay">
             <span class="autoplay-title">Ads</span>
           </p>
-          bbbb
+          
+          <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+          <!-- TravelpearNew01 -->
+          <ins class="adsbygoogle"
+               style="display:block"
+               data-ad-client="ca-pub-0555574208214622"
+               data-ad-slot="6171108875"
+               data-ad-format="auto"
+               data-full-width-responsive="true"></ins>
+          <script>
+          (adsbygoogle = window.adsbygoogle || []).push({});
+          </script>
 
 
         </div>
